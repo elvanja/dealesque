31b2952f1f67336eabeb8d52085e24f117c906be
Vanja Radovanović <elvanja@gmail.com>
2013-01-05 20:59:16 +0100

    enabled getting amazon configuration from environment

--- config/initializers/dealesque.rb
+++ config/initializers/dealesque.rb
@@ -2,7 +2,39 @@ require 'yaml'
 
 class ConfigurationError < StandardError; end
 
-credentials_file = "config/amazon.yml"
-raise ConfigurationError("Missing credentials file, expected to be at #{File.expand_path(credentials_file)}") unless File.exists?(credentials_file)
+def validate_credentials(credentials, environment)
+  errors = []
+  errors << "Missing amazon key in #{environment}" unless credentials["key"]
+  errors << "Missing amazon secret in #{environment}" unless credentials["secret"]
+  errors << "Missing amazon tag in #{environment}" unless credentials["tag"]
+  errors
+end
 
-AMAZON_CREDENTIALS = YAML::load(File.open(credentials_file))[Rails.env]
+def get_credentials_from_environment
+  credentials = {
+      "key" => ENV['AMAZON_KEY'],
+      "secret" => ENV['AMAZON_SECERET'],
+      "tag" => ENV['AMAZON_TAG']
+  }
+  return credentials, validate_credentials(credentials, :environment)
+end
+
+def get_credentials_from_file
+  credentials = {}
+  file = "config/amazon.yml"
+  if File.exists?(file)
+    credentials = YAML::load(File.open(file))[Rails.env]
+    errors = validate_credentials(credentials, file)
+  else
+    errors = ["Missing credentials file @ #{File.expand_path(file)})"]
+  end
+  return credentials, errors
+end
+
+credentials, errors = get_credentials_from_environment
+if errors.size > 0
+  credentials, additional_errors = get_credentials_from_file
+  raise ConfigurationError.new((errors + additional_errors).join("\n")) if additional_errors.size > 0
+end
+
+AMAZON_CREDENTIALS = credentials
