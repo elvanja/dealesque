087bfc714c548976d5101a6fba6aa6df42550855
Vanja Radovanović <elvanja@gmail.com>
2013-01-05 23:23:33 +0100

    added the script to deploy to herokui

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-git checkout -b deploy_to_heroku
+git flow release start deploy_to_heroku
+if [ $? -ne 0 ]; then
+  echo "You must have a clean branch to deploy to heroku"
+  exit
+fi
 RAILS_ENV=production rake assets:precompile
 git add -A .
 git commit -m "added precompiled assets"
+#git push -f heroku HEAD:master
+git checkout develop
+git branch -D feature/deploy_to_heroku
