194d5cf465d1c1befb7d339044ab0d74a5c2597f
Vanja Radovanović <elvanja@gmail.com>
2013-04-29 23:14:51 +0200

    added a TODO to remind of scraping in background

--- app/use_cases/pick_item.rb
+++ app/use_cases/pick_item.rb
@@ -9,6 +9,7 @@ class PickItem
   end
 
   def pick(item)
+    # TODO scrape in background and add to item in callback (or in some other way), scraping is too slow for the user to wait
     item.append_offers(@item_offer_listing_scraper.scrape_offers_for(item))
     @picked_items_container.add(item)
   end
